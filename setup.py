ef552048bc12fd77bb6c9f391c917303afd6180a
Caterina Urban <caterina.urban@gmail.com>
2017-02-16 09:26:12 +0100

    project structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Lyra',
+    'author': 'Caterina Urban',
+    'url': 'https://github.com/caterinaurban/Lyra',
+    'download_url': 'https://github.com/caterinaurban/Lyra',
+    'author_email': 'caterina.urban@gmail.com',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': ['lyra'],
+    'scripts': [],
+    'name': 'Lyra'
+}
+
+setup(**config)
