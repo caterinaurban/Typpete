73332acd04a491f9147ba23c56c375db1d013794
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2017-03-20 10:54:33 +0100

    Implemented dictionary comprehension

--- frontend/context.py
+++ frontend/context.py
@@ -7,13 +7,14 @@ class Context:
 
     def __init__(self, t_m={}, parent_context=None):
         self.types_map = t_m
+        self.parent_context = parent_context
 
     def get_type(self, var_name):
         if var_name in self.types_map:
             return self.types_map[var_name]
-        if parent_context == None:
+        if self.parent_context == None:
             raise NameError("Name {} is not defined.".format(var_name))
-        return parent_context.get_type(var_name)
+        return self.parent_context.get_type(var_name)
 
     def set_type(self, var_name, var_type):
         """Sets the type of a variable in this context."""
