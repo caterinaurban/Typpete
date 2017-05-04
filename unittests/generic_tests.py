908d83ef33c7f2dbab2877df65911bc4a1285176
Simon Wehrli <wehrli.simon@gmail.com>
2017-05-04 15:13:51 +0200

    class based (generic) unittests
    extract common testcase functionality in super classes
    it is now easy to create new test cases for specific analysis, giving you the freedom of running pre/post analysis but providing the uninteresting boilerplate code.

---
+++ unittests/generic_tests.py
@@ -0,0 +1,117 @@
+import io
+import tokenize
+import os
+import re
+import unittest
+from abc import abstractmethod
+import ast
+
+from frontend.cfg_generator import ast_to_cfg
+from visualization.graph_renderer import CfgRenderer, AnalysisResultRenderer
+
+
+def source_path_to_name(source_path):
+    return os.path.splitext(os.path.basename(source_path))[0]
+
+
+class FileTestCase(unittest.TestCase):
+    def __init__(self, source_path):
+        super().__init__()
+        self._source_path = source_path
+        self._source_directory = os.path.dirname(self._source_path)
+        self._name = source_path_to_name(self._source_path)
+
+    def setUp(self):
+        with open(self._source_path, 'r') as source_file:
+            self._source = source_file.read()
+        self._ast_root = ast.parse(self.source)
+        self._cfg = ast_to_cfg(self.ast_root)
+
+    def render_cfg(self):
+        CfgRenderer().render(self.cfg, label=f"CFG for {self.source_path}", filename=f"CFG {self.name}",
+                             directory=os.path.join(self._source_directory, "graphs"),
+                             view=False)
+
+    def render_result_cfg(self, result):
+        AnalysisResultRenderer().render((self.cfg, result), label=f"CFG with Results for {self._source_path}",
+                                        filename=f"CFGR {self.name}",
+                                        directory=os.path.join(self._source_directory, "graphs"), view=False)
+
+    @property
+    def source_path(self):
+        return self._source_path
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def ast_root(self):
+        return self._ast_root
+
+    @property
+    def source(self):
+        return self._source
+
+    @property
+    def cfg(self):
+        return self._cfg
+
+    @abstractmethod
+    def runTest(self):
+        """
+        Specific test logic.
+        """
+
+    def __str__(self):
+        return f"Unit test for Python source at {self._source_path}"
+
+
+class ResultCommentsFileTestCase(FileTestCase):
+    def __init__(self, source_path):
+        super().__init__(source_path)
+
+    @abstractmethod
+    def runTest(self):
+        """
+        Specific test logic.
+        """
+
+    def check_result_comments(self, result):
+        for line, expected_result in self._find_result_comments():
+            actual_result = self._find_analysis_result_for_comments(result, line)
+            actual_result_str = str(actual_result)
+            self.assertEqual(expected_result, actual_result_str,
+                             f"expected != actual result at line {line}")
+
+    def _find_result_comments(self):
+        # Parse comments to find expected results
+        pattern = re.compile('RESULT:?\s*(?P<result>.*)')
+        for t in tokenize.tokenize(io.BytesIO(self.source.encode('utf-8')).readline):
+            if t.type == tokenize.COMMENT:
+                comment = t.string.strip("# ")
+                m = pattern.match(comment)
+                if m:
+                    expected_result = m.group('result')
+                    line = t.start[0]
+                    yield line, expected_result
+
+    def _find_analysis_result_for_comments(self, result, line_of_comment):
+        # search for analysis result of a statement with the same source line
+        actual_result = None
+        for node in self.cfg.nodes.values():
+            states = result.get_node_result(node)
+
+            for i, stmt in enumerate(node.stmts):
+                if stmt.pp.line == line_of_comment:
+                    actual_result = states[i]
+                    break
+
+            if actual_result:
+                break
+
+        if actual_result:
+            return actual_result
+        else:
+            raise RuntimeError(
+                f"No analysis result found for RESULT-comment '{comment}' at line {line}!")
--- unittests/usage_tests.py
+++ unittests/usage_tests.py
@@ -1,48 +1,31 @@
 import glob
-import unittest
-import ast
 from abstract_domains.usage.stack import UsedStack
 from core.expressions import VariableIdentifier
 from engine.backward import BackwardInterpreter
-from frontend.cfg_generator import ast_to_cfg
 from semantics.usage.usage_semantics import UsageSemantics
-from visualization.graph_renderer import CfgRenderer, AnalysisResultRenderer
-from unittests.test_tools import *
+from unittests.generic_tests import *
 
 
-class UsageTestCase(unittest.TestCase):
+class UsageTestCase(ResultCommentsFileTestCase):
     def __init__(self, source_path):
-        super().__init__()
+        super().__init__(source_path)
         self._source_path = source_path
 
     def runTest(self):
-        name = source_path_to_name(self._source_path)
-        print(f"Start test for Python source at {self._source_path}")
-
-        with open(self._source_path, 'r') as source_file:
-            source = source_file.read()
-        root = ast.parse(source)
-        cfg = ast_to_cfg(root)
-        CfgRenderer().render(cfg, label=f"CFG for {self._source_path}", filename=f"CFG {name}", directory="graphs",
-                             view=False)
+        print(self)
+        self.render_cfg()
 
         # find all variables
         variable_names = sorted(
-            {node.id for node in ast.walk(root) if
+            {node.id for node in ast.walk(self.ast_root) if
              isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store)})
         variables = [VariableIdentifier(int, name) for name in variable_names]
 
         # Run Usage Analysis
-        backward_interpreter = BackwardInterpreter(cfg, UsageSemantics(), 3)
+        backward_interpreter = BackwardInterpreter(self.cfg, UsageSemantics(), 3)
         result = backward_interpreter.analyze(UsedStack(variables))
-        AnalysisResultRenderer().render((cfg, result), label=f"CFG with Results for {self._source_path}",
-                                        filename=f"CFGR {name}", directory="graphs", view=False)
-
-        for line, expected_result in find_result_comments(source):
-            actual_result = find_analysis_result_for_comments(cfg, result, line)
-            actual_result_str = str(actual_result)
-            self.assertEqual(expected_result, actual_result_str,
-                             f"expected != actual result at line {line}")
+        self.render_result_cfg(result)
+        self.check_result_comments(result)
 
 
 def suite():
