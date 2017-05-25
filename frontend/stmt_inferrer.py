1f1d362bcce986d7c5623483b09102495be6fb35
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2017-05-25 17:15:10 +0200

    Added docstrings to builtins stub

--- frontend/stmt_inferrer.py
+++ frontend/stmt_inferrer.py
@@ -324,8 +324,8 @@ def _infer_func_def(node, context, solver):
     body_type = _infer_body(node.body, func_context, node.lineno, solver)
     if node.returns:
         return_type = solver.resolve_annotation(unparse_annotation(node.returns))
-
-        if len(node.body) == 1 and isinstance(node.body[0], ast.Pass):
+        if ((len(node.body) == 1 and isinstance(node.body[0], ast.Pass))
+           or (len(node.body) == 2 and isinstance(node.body[0], ast.Expr) and isinstance(node.body[1], ast.Pass))):
             # Stub function
             body_type = return_type
         else:
