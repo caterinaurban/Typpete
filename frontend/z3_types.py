3f38e62f39c34b301b6a139d550218178d7fd3cf
Caterina Urban <caterina.urban@gmail.com>
2017-07-21 14:42:11 +0200

    putting things back

--- frontend/z3_types.py
+++ frontend/z3_types.py
@@ -66,6 +66,7 @@ class Z3Types:
         # Encode subtyping relationships
         self.subtype = Function("subtype", type_sort, type_sort, BoolSort())
         self.extends = Function("extends", type_sort, type_sort, BoolSort())
+        self.not_subtype = Function("not subtype", type_sort, type_sort, BoolSort())
 
         x = Const("x", type_sort)
         y = Const("y", type_sort)
@@ -78,7 +79,8 @@ class Z3Types:
             ForAll([x, y], Implies(And(self.subtype(x, y), self.subtype(y, x)), x == y)),
             ForAll([x, y, z],
                    Implies(And(self.extends(x, z), self.extends(y, z), x != y),
-                           And(Not(self.subtype(x, y)), Not(self.subtype(y, x))))),
+                           And(self.not_subtype(x, y), self.not_subtype(y, x)))),
+            ForAll([x, y, z], Implies(And(self.subtype(x, y), self.not_subtype(y, z)), Not(self.subtype(x, z)))),
         ]
 
         self.generics_axioms = [
