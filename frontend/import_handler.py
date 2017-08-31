a29ef15930eb959e24f8144269ef5f74d8c4ef55
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2017-08-31 10:47:16 +0200

    Cache imports at first time

--- frontend/import_handler.py
+++ frontend/import_handler.py
@@ -6,6 +6,7 @@ from frontend.stubs.stubs_paths import libraries
 class ImportHandler:
     """Handler for importing other modules during the type inference"""
     cached_asts = {}
+    cached_modules = {}
 
     @staticmethod
     def get_ast(path, module_name):
@@ -43,17 +44,22 @@ class ImportHandler:
     @staticmethod
     def infer_import(module_name, base_folder, infer_func, solver):
         """Infer the types of a python module"""
+        if module_name in ImportHandler.cached_modules:
+            # Return the cached context if this module is already inferred before
+            return ImportHandler.cached_modules[module_name]
         if ImportHandler.is_builtin(module_name):
-            return solver.stubs_handler.infer_builtin_lib(module_name, solver,
-                                                          solver.config.used_names, infer_func)
+            ImportHandler.cached_modules[module_name] = solver.stubs_handler.infer_builtin_lib(module_name,
+                                                                                               solver,
+                                                                                               solver.config.used_names,
+                                                                                                           infer_func)
         else:
             t = ImportHandler.get_module_ast(module_name, base_folder)
             context = Context(t.body, solver)
             solver.infer_stubs(context, infer_func)
             for stmt in t.body:
                 infer_func(stmt, context, solver)
-
-        return context
+            ImportHandler.cached_modules[module_name] = context
+        return ImportHandler.cached_modules[module_name]
 
     @staticmethod
     def is_builtin(module_name):
