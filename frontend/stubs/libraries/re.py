f377018fb313493b272cdd379f6104f7bb291bfb
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2018-01-19 15:13:39 +0200

    Enhanced re stub

--- frontend/stubs/libraries/re.py
+++ frontend/stubs/libraries/re.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union, TypeVar, Tuple, Callable
 
 # ----- re variables and constants -----
 DEBUG = 0
@@ -16,6 +16,8 @@ U = 0
 UNICODE = 0
 T = 0
 TEMPLATE = 0
+
+Any = TypeVar('Any')
 
 class Match:
     pos = 0
@@ -46,5 +48,25 @@ class Pattern:
 
     def match(self, string: str, pos: int = 1, endpos: int = 1) -> Match: ...
 
-def compile(pattern: str, flags: int = 1) -> Pattern: ...
+def compile(pattern: Union[Pattern, str], flags: int = 1) -> Pattern: ...
 
+def search(pattern: Union[Pattern, str], string: str, flags: int = 1) -> Match: ...
+
+def match(pattern: Union[Pattern, str], string: str, flags: int = 1) -> Match: ...
+
+def fullmatch(pattern: Union[Pattern, str], string: str, flags: int = 1) -> Match: ...
+
+def split(pattern: Union[Pattern, str], maxsplit: int = 1, flags: int = 1) -> Match: ...
+
+def findall(pattern: Union[Pattern, str], maxsplit: int = 1, flags: int = 1) -> List[Any]: ...
+
+def sub(pattern: Union[Pattern, str], repl: Union[str, Callable[[Match], str]],
+        string: str, count: int = 0, flags: int = 0) -> str: ...
+
+def subn(pattern: Union[Pattern, str], repl: Union[str, Callable[[Match], str]],
+         string: str, count: int = 0, flags: int = 0) -> Tuple[str, int]: ...
+
+
+def escape(string: str) -> str: ...
+
+def template(pattern: Union[str, Pattern], flags: int = 1) -> Pattern: ...
